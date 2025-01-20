e8eb7682b98d89e85f290e0137307dbc29b21e95
Shibbir Ahmad <62713622+shibbir-ahmad24@users.noreply.github.com>
2025-01-20 02:55:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name = 'Medical Chatbot',
     version= '0.0.0',
-    author= 'Dushyant Nagar',
+    author= 'Shibbir Ahmed',
     author_email= 'shibbir282@gmail.com',
     packages= find_packages(),
     install_requires = []
